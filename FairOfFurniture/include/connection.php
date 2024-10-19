a7efc72a5d8932d6f359e0995e1fd2eee799ba53
Mariaa888 <142675401+Mariaa888@users.noreply.github.com>
2024-10-19 19:46:17 +0300

    Delete FairOfFurniture directory

--- FairOfFurniture/include/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect('localhost', 'root', '', 'fairoffurniture');
-if (!$con) {
-    die('error' . mysqli_connect_error());
-} else {
- 
-}
--- FairOfFurniture/js/home1.js
+++
@@ -1,8 +0,0 @@
-let bar = document.querySelector(".navbar");
-function show() {
-
-    bar.style.display = "flex";
-}
-function closebar() {
-    bar.style.display = 'none';
-}   A
